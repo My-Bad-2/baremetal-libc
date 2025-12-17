336ae4cbacf645de5646309d23def160cc341dac
Yash Verma <yashverma056@proton.me>
2025-12-17 02:21:46 -0500

    Tweak stdint.h for gcc cross compiler support

--- include/stdint.h
+++ include/stdint.h
@@ -9,6 +9,10 @@
 #ifndef LLVM_LIBC_STDINT_H
 #define LLVM_LIBC_STDINT_H
 
+#ifndef __clang__
+#include_next <stdint.h>
+#else
 #include "llvm-libc-macros/stdint-macros.h"
+#endif
 
 #endif // LLVM_LIBC_STDINT_H
